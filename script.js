1b3115d5b4bbf312124b60f37b3049660a1c97b3
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-06 15:50:46 +0200

    milestone 1

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return {};
+        return {
+            toDoList: [
+                { text: "Fare la spesa", completed: false },
+                { text: "Pulire la casa", completed: false },
+                { text: "Preparare la cena", completed: false },
+                { text: "Fare esercizio fisico", completed: false },
+                { text: "Leggere un libro", completed: false },
+            ],
+        };
     },
-});
+    methods: {
+        toggleTask(element, index) {
+            element = this.toDoList[index];
+            element.completed = !element.completed;
+
+            console.log("funzia");
+            return element;
+        },
+    },
+}).mount("#app");
